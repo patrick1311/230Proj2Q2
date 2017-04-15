3d7e37b7e65bd19b031d5881e2a6bc88590694f8
justintim3 <justlin90@gmail.com>
2017-04-15 16:14:05 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,79 +1,306 @@
-/*
-Project Name: Sorting Algorithms (main.cpp)
-Programmer Names: Justin Lin, Princeton Wong, Patrick Quach
-Class: CSCI 230
-Project Number: 1
-Date: 3/28/2017
-Project Description: This application runs various sorting algorithms and outputs their sort times to console and textfile.
-*/
-
-#include <cmath>	//pow
-#include <iostream>	//cout
-#include <string>	//string
-#include <iomanip>	//setw formatting
-#include <fstream>	//output textfile
-#include <chrono>	//high-resolution clock
-#include "bubsort.cpp"
-#include "heapsort.cpp"
-#include "heapsortO.cpp"
-#include "inssort.cpp"
-#include "mrgsort.cpp"
-#include "mrgsortO.cpp"
-#include "qsort.cpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>	//time
+#include <limits>	//GoToLine
+//#include "ReplacementSelection.cpp"
+#include "heap.h"
+#include "Comp.cpp"
+#include "heap.h"
 #include "qsortop.cpp"
-#include "radix.cpp"
-#include "selsort.cpp"
-#include "shellsort.cpp"
-#include "shellsortO.cpp"
+//#pragma once
 
-using namespace std;
-using namespace std::chrono;
+int THRESHOLD2 = 8;
+bool done = false;
+int eofLine = 7;
 
-template<typename E> void fillArrAsc(E a[], int size);	//Fills array with values 0-n in ascending order
-template<typename E> void initializeArr(E a[], int size);	//Initiate array with values 0
-template<typename E> void copyArr(E a[], E b[], int size);	//Copies array b into array a
-template<typename E> void shuffleArr(E a[], int size);	//Randomly shuffles an array
-template<typename E> void calculateTimes(const int sizeStart, const int sizeEnd, int loopNumber, E insTimes[],
-	E bubTimes[], E selTimes[], E shellTimes[], E shellOTimes[], E mrgTimes[], E mrgOTimes[],
-	E qckTimes[], E qckOTimes[], E heapTimes[], E heapOTimes[], E rad4Times[], E rad8Times[]);
-	//Calculates runtimes of sorting algorithms at various array sizes and stores the data inside arrays
-template<typename E> void printTable(int sizeStart, int sizeEnd, E insTimes[], E bubTimes[], E selTimes[],
-	E shellTimes[], E shellOTimes[], E mrgTimes[], E mrgOTimes[], E qckTimes[],
-	E qckOTimes[], E heapTimes[], E heapOTimes[], E rad4Times[], E rad8Times[]);
-	//Prints table of runtimes for different sorting algorithms at different sized arrays
-template<typename E> double timeRecorder(void sort(E *, int), E *arr, int size);	//Records sorting algorithm runtime
-template<typename E> void printSort(E arr[], int printStart, int printEnd, ofstream &textFile, std::string sortName);
-	//Prints sort
+void genRandFile(std::string fileName, int lines);
+void genRandFile2(std::string fileName, int lines);
+template<typename E> void fillArrAsc(E a[], int size);
+template<typename E> void shuffleArr(E a[], int size);
+std::streamoff readData(std::string fileName, std::string *inBuff, std::streamoff start, int size);
+template<typename E> void printArr(E a[], int size);
+template<typename E> void printHeap(heap<E, Comp<E>>* heap, int size);
+template<typename E> void repSel(std::string inFile , std::string outFile, heap<E, Comp<E>>* minHeap, std::streamoff start, int buffSize);
+void clearData(std::string fileName);
+template<typename E> void writeData(std::string fileName, E *outBuff, int size);
+template<typename E> void multiMrg(std::string fileName, int buffSize, int numRuns);
+std::fstream& goToLine(std::fstream& file, unsigned int num);
+template<typename E> void initializeArr(E a[], int size);	//Initiates array with value 0
 
-int THRESHOLD1 = 4, THRESHOLD2 = 8;	//THRESHOLD1 used for radix sort/4; THRESHOLD2 used for radix sort/8
-int pow2[9] = {1, 2, 4, 8, 16, 32, 64, 128, 256};	//Used for radix sort
-
-int main() {
+int main()
+{
 	srand((unsigned)time(0));
-	const int sizeStart = 1, sizeEnd = 1;	//sizeStart and sizeEnd determine the range of array sizes to calculate runtimes for
-	int loopNumber = 100000;	//Determines the number of times to loop to average the runtimes for sorts
-	double insTimes[sizeEnd - sizeStart + 1], bubTimes[sizeEnd - sizeStart + 1], selTimes[sizeEnd - sizeStart + 1],
-		shellTimes[sizeEnd - sizeStart + 1], shellOTimes[sizeEnd - sizeStart + 1], mrgTimes[sizeEnd - sizeStart + 1],
-		mrgOTimes[sizeEnd - sizeStart + 1], qckTimes[sizeEnd - sizeStart + 1], qckOTimes[sizeEnd - sizeStart + 1],
-		heapTimes[sizeEnd - sizeStart + 1], heapOTimes[sizeEnd - sizeStart + 1], rad4Times[sizeEnd - sizeStart + 1],
-		rad8Times[sizeEnd - sizeStart + 1];
-	calculateTimes(sizeStart, sizeEnd, loopNumber, insTimes,
-		bubTimes, selTimes, shellTimes, shellOTimes, mrgTimes, mrgOTimes,
-		qckTimes, qckOTimes, heapTimes, heapOTimes, rad4Times, rad8Times);
-	printTable(sizeStart, sizeEnd, insTimes, bubTimes, selTimes,
-		shellTimes, shellOTimes, mrgTimes, mrgOTimes, qckTimes,
-		qckOTimes, heapTimes, heapOTimes, rad4Times, rad8Times);
+	int bufferSize = eofLine, heapSize = 7;
+	std::streamoff start;
+	std::string inputFile = "RandomData.txt", outputFile = "SortedData.txt";
 
-	string buffer;
-	cin >> buffer;	//Pause console after program finishes
+	std::string *heapArr = new std::string[heapSize];
+	clearData(outputFile);	//clear output file
+	genRandFile(inputFile, heapSize + (bufferSize *3) + 4);	//generate input file
+	start = readData(inputFile, heapArr, 0, heapSize);	//read data into heap array
+	heap<std::string, Comp<std::string>>* minHeap = new heap<std::string, Comp<std::string>>(heapArr, heapSize, heapSize);
+		std::cout << "heap___________________________________" << std::endl;
+		printArr(heapArr, heapSize);
+	repSel<std::string>(inputFile, outputFile, minHeap, start, bufferSize);
+	qsortO(heapArr, heapSize);
+		std::cout << "heap___________________________________" << std::endl;
+		printArr(heapArr, heapSize);
+	writeData(outputFile, heapArr, heapSize);	//write array to output file
+	//multiMrg<std::string>(outputFile, heapSize + (bufferSize + 2));
+
+	std::string wait;
+	std::cin >> wait;	//Pause console after program finishes
 	return 0;
+}
+
+void genRandFile(std::string fileName, int lines)
+{
+	std::ofstream ofs;
+	ofs.open(fileName);
+
+	int randLines = lines;//(rand() % 100) + 60;
+	int randLength, maxLength = 20;
+	char randChar;
+	for (int i = 0; i < randLines; i++)
+	{
+		randLength = (rand() % maxLength) + 1;
+		for (int j = 0; j < randLength; j++)
+		{
+			randChar = (rand() % 26) + 'A';
+			ofs << randChar;
+		}
+		ofs << std::endl;
+	}
+	ofs.close();
+}
+
+void genRandFile2(std::string fileName, int lines)
+{
+	std::ofstream ofs;
+	ofs.open(fileName);
+
+	std::string *stringArr = new std::string[lines];
+	fillArrAsc(stringArr, lines);
+	shuffleArr(stringArr, lines);
+
+	for (int i = 0; i < lines; i++)
+	{
+		ofs << stringArr[i] << std::endl;
+	}
+	ofs.close();
+	delete[] stringArr;
 }
 
 template<typename E>
 void fillArrAsc(E a[], int size) {	//Fills array with values 0-n in ascending order
-	for (int i = 0; i < size; i++){
-		a[i] = i;
+	for (int i = 0; i < size; i++) {
+		a[i] = i + 65;
 	}
+}
+
+template<typename E>
+void shuffleArr(E a[], int size) {	//Randomly shuffles an array
+	int num;
+	for (int i = 0; i < size; i++) {
+		num = rand() % size;
+		swap(a[i], a[num]);
+	}
+}
+
+std::streamoff readData(std::string fileName, std::string *inBuff, std::streamoff start, int size)
+{
+	std::streamoff getPos;
+	std::ifstream ifs(fileName);
+
+	if (ifs.is_open())
+	{
+		ifs.seekg(start);
+		//while(!stream.eof())
+		for (int i = 0; i < size; i++)
+		{
+			if (ifs.eof())
+			{
+				eofLine = i - 1;	//record line number of eof
+				std::cout << "line number is: " << eofLine << std::endl;
+				done = true;
+				break;	//stop recording into input buffer
+			}
+			//while (getline(ifs, buffer[i]))
+			getline(ifs, inBuff[i]);
+		}
+	}
+	getPos = ifs.tellg();
+	//printArr(inBuff, size);
+	ifs.close();
+
+	return getPos;
+	//delete buffer;
+}
+
+void clearData(std::string fileName)
+{
+	std::ofstream ofs(fileName, std::ios::out | std::ios::trunc);
+	ofs.close();
+}
+
+template<typename E>
+void writeData(std::string fileName, E *outBuff, int size)
+{
+	std::ofstream ofs(fileName, std::ios::out | std::ios::app);
+
+	if (ofs.is_open())
+	{
+		//ofs.seekp(start);
+		//while(!stream.eof())
+		for (int i = 0; i < size; i++)
+		{
+			//while (getline(ifs, buffer[i]))
+			ofs << outBuff[i] << std::endl;
+		}
+	}
+	//printArr(outBuff, size);
+	ofs.close();
+}
+
+template<typename E>
+void printArr(E a[], int size) {	//Prints an array
+	for (int i = 0; i < size; i++) {
+		std::cout << a[i] << std::endl;
+	}
+}
+
+template<typename E>
+void printHeap(heap<E, Comp<E>>* heap, int size) {	//Prints a heap
+	for (int i = 0; i < size; i++) {
+		std::cout << heap->getVal(i) << std::endl;
+	}
+}
+
+template<typename E>
+void repSel(std::string inFile, std::string outFile, heap<E, Comp<E>>* minHeap, std::streamoff start, int buffSize)
+{
+	E *inBuff = new E[buffSize];
+	E *outBuff = new E[buffSize];
+	int heapSize = minHeap->size();
+
+	do//for (int i = 0; i < 2; i++)
+	{
+		//std::cout << "start" << start << std::endl;
+		start = readData(inFile, inBuff, start, buffSize);
+		//std::cout << "start" << start << std::endl;
+		for (int j = 0; j < eofLine; j++)
+		{
+			if (minHeap->size() == 0)	//if heap size is 0, rebuild heap
+			{
+				minHeap->setHeapSize(heapSize);
+				minHeap->buildHeap();
+				//std::cout << minHeap->size() << "buildHeap_______________________" << std::endl;
+			}
+
+			outBuff[j] = minHeap->getVal(0);	//send root to heap buffer
+			minHeap->setVal(0, inBuff[j]);
+			if (inBuff[j] > outBuff[j])
+			{
+				minHeap->siftdown(0);	//siftdown root
+				//std::cout << minHeap->size() << "size_______________________" << std::endl;
+
+			}
+			else
+			{
+				minHeap->removefirst();
+				//std::cout << minHeap->size() << "size_______________________" << std::endl;
+			}
+		}
+
+		writeData(outFile, outBuff, eofLine);
+
+		std::cout << "in___________________________________" << std::endl;
+		printArr(inBuff, buffSize);
+		std::cout << "out___________________________________" << std::endl;
+		printArr(outBuff, eofLine);
+
+		minHeap->setHeapSize(heapSize);
+		minHeap->buildHeap();
+		//std::cout << minHeap->size() << "buildHeap_______________________" << std::endl;
+	} while (!done);
+	delete[] inBuff;
+	delete[] outBuff;
+}
+
+template<typename E>
+void multiMrg(std::string inFile, std::string outFile, int buffSize, int numRuns)	//Temporarily using quicksort for testing
+{
+	std::ifstream ifs(inFile);
+	//std::ifstream ofs(outFile);
+	int size = numRuns + 1;
+	E *inBuff = new E[size];
+	E runStart = new E[size];
+	initializeArr(runStart, numRuns + 1);
+	
+	if (ifs.is_open())
+	{
+		for (int i = 0; i < size; i++)	//store values from each run into array
+		{
+			inBuff[i] = firstElement(inFile, i + 1, runStart[i], buffSize);
+		}
+
+		//write smallest value to file
+		writeData(outFile, smallestElement(arr, size), 1);
+		runStart[]++;
+
+
+
+	}
+
+	/*
+	readData(fileName, inBuff, 0, buffSize);
+
+	qsortO(inBuff, buffSize);
+
+	clearData(fileName);
+	writeData(fileName, inBuff, buffSize);*/
+
+	delete[] inBuff;
+	ifs.close();
+}
+
+template<typename E>
+E firstElement(std::string fileName, int run, int start, int buffSize)//returns first element of run
+{
+	std::ifstream ifs(fileName);
+	E first;
+	if (ifs.is_open())
+	{
+		goToLine(ifs, buffSize * run + start);
+		getline(ifs, first);
+	}
+	ifs.close();
+	return first;
+}
+
+template<typename E>
+E smallestElement(E arr[], int size)	//returns smallest element in array
+{
+	E smallest = arr[0];
+
+	for (int i = 1; i < size; i++)
+	{
+		if (arr[i] < smallest)
+		{
+			smallest = arr[i];
+		}
+	}
+	return smallest;
+}
+
+std::fstream& goToLine(std::fstream& file, unsigned int num) {
+	//Sets the seek pointer of file to the beginning of line num
+	file.seekg(std::ios::beg);
+	for (int i = 0; i < num - 1; i++) {
+		file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+	return file;
 }
 
 template<typename E>
@@ -81,162 +308,4 @@ void initializeArr(E a[], int size) {	//Initiates array with value 0
 	for (int i = 0; i < size; i++) {
 		a[i] = 0;
 	}
-}
-
-template<typename E>
-void copyArr(E a[], E b[], int size) {	//Copies array b into array a
-	for (int i = 0; i < size; i++) {
-		a[i] = b[i];
-	}
-}
-
-template<typename E>
-void shuffleArr(E a[], int size) {	//Randomly shuffles an array
-	int num;
-	for (int i = 0; i < size; i++){
-		num = rand() % size;
-		swap(a[i], a[num]);
-	}
-}
-
-template<typename E>
-void calculateTimes(const int sizeStart, const int sizeEnd, int loopNumber, E insTimes[], 
-	E bubTimes[], E selTimes[], E shellTimes[], E shellOTimes[], E mrgTimes[], E mrgOTimes[], 
-	E qckTimes[], E qckOTimes[], E heapTimes[], E heapOTimes[], E rad4Times[], E rad8Times[])
-{	//Calculates runtimes of sorting algorithms at various array sizes and stores the data inside arrays
-	initializeArr(insTimes, sizeEnd - sizeStart + 1);	//Initiate array with values 0
-	initializeArr(bubTimes, sizeEnd - sizeStart + 1);
-	initializeArr(selTimes, sizeEnd - sizeStart + 1);
-	initializeArr(shellTimes, sizeEnd - sizeStart + 1);
-	initializeArr(shellOTimes, sizeEnd - sizeStart + 1);
-	initializeArr(mrgTimes, sizeEnd - sizeStart + 1);
-	initializeArr(mrgOTimes, sizeEnd - sizeStart + 1);
-	initializeArr(qckTimes, sizeEnd - sizeStart + 1);
-	initializeArr(qckOTimes, sizeEnd - sizeStart + 1);
-	initializeArr(heapTimes, sizeEnd - sizeStart + 1);
-	initializeArr(heapOTimes, sizeEnd - sizeStart + 1);
-	initializeArr(rad4Times, sizeEnd - sizeStart + 1);
-	initializeArr(rad8Times, sizeEnd - sizeStart + 1);
-
-	for (int i = sizeStart; i <= sizeEnd; i++)
-	{
-		int size = (int)pow(10.0, (double)i);
-		int *arr = new int[size], *temp = new int[size];
-
-		fillArrAsc(arr, size);	//Initialize array in ascending order
-
-		for (int j = 0; j < loopNumber; j++)
-		{
-			shuffleArr(arr, size);	//Shuffle array randomly
-			copyArr(temp, arr, size);	//Duplicate array to test the same random array across sorts
-
-			insTimes[i - sizeStart] += timeRecorder(&inssort, arr, size);
-			copyArr(arr, temp, size);	//Restore array to same random shuffle
-			bubTimes[i - sizeStart] += timeRecorder(&bubsort, arr, size);
-			copyArr(arr, temp, size);
-			selTimes[i - sizeStart] += timeRecorder(&selsort, arr, size);
-			copyArr(arr, temp, size);
-			shellTimes[i - sizeStart] += timeRecorder(&shellsort, arr, size);
-			copyArr(arr, temp, size);
-			shellOTimes[i - sizeStart] += timeRecorder(&shellsortO, arr, size);
-			copyArr(arr, temp, size);
-			mrgTimes[i - sizeStart] += timeRecorder(&mrgsort, arr, size);
-			copyArr(arr, temp, size);
-			mrgOTimes[i - sizeStart] += timeRecorder(&mrgsortO, arr, size);
-			copyArr(arr, temp, size);
-			qckTimes[i - sizeStart] += timeRecorder(&qsort, arr, size);
-			copyArr(arr, temp, size);
-			qckOTimes[i - sizeStart] += timeRecorder(&qsortO, arr, size);
-			copyArr(arr, temp, size);
-			heapTimes[i - sizeStart] += timeRecorder(&heapsort, arr, size);
-			copyArr(arr, temp, size);
-			heapOTimes[i - sizeStart] += timeRecorder(&heapsortO, arr, size);
-			copyArr(arr, temp, size);
-			THRESHOLD1 = 4;
-			rad4Times[i - sizeStart] += timeRecorder(&radixsort, arr, size);
-			copyArr(arr, temp, size);
-			THRESHOLD1 = 8;
-			rad8Times[i - sizeStart] += timeRecorder(&radixsort, arr, size);
-		}
-
-		insTimes[i - sizeStart] /= loopNumber;	//Divide by loopNumber to average sort runtimes
-		bubTimes[i - sizeStart] /= loopNumber;
-		selTimes[i - sizeStart] /= loopNumber;
-		shellTimes[i - sizeStart] /= loopNumber;
-		shellOTimes[i - sizeStart] /= loopNumber;
-		mrgTimes[i - sizeStart] /= loopNumber;
-		mrgOTimes[i - sizeStart] /= loopNumber;
-		qckTimes[i - sizeStart] /= loopNumber;
-		qckOTimes[i - sizeStart] /= loopNumber;
-		heapTimes[i - sizeStart] /= loopNumber;
-		heapOTimes[i - sizeStart] /= loopNumber;
-		rad4Times[i - sizeStart] /= loopNumber;
-		rad8Times[i - sizeStart] /= loopNumber;
-
-		delete [] arr, temp;
-	}
-}
-
-template<typename E>
-double timeRecorder(void sort(E *, int), E *arr, int size)
-{	//Records sorting algorithm runtime
-	high_resolution_clock::time_point t1, t2;
-	double time;
-
-	t1 = high_resolution_clock::now();
-	sort(arr, size);
-	t2 = high_resolution_clock::now();
-
-	time = (double)duration_cast<nanoseconds>(t2 - t1).count() / 1000000;	//Divide by 1000000 to convert to miliseconds
-	return time;
-}
-
-template<typename E>
-void printTable(int sizeStart, int sizeEnd, E insTimes[], E bubTimes[], E selTimes[], 
-	E shellTimes[], E shellOTimes[], E mrgTimes[], E mrgOTimes[], E qckTimes[],
-	E qckOTimes[], E heapTimes[], E heapOTimes[], E rad4Times[], E rad8Times[])
-{	//Prints table of runtimes for different sorting algorithms at different sized arrays to console and textfile
-	ofstream textFile;
-	textFile.open("SortingData.txt");
-
-	std::cout << setw(10) << "Sort";
-	textFile << "Sort";
-
-	for (int i = sizeStart; i <= sizeEnd; i++)
-	{
-		std::cout << setw(12) << (int)pow(10, (double)(i));
-		textFile << "\t" << (int)pow(10, (double)(i));
-	}
-	std::cout << endl;
-	textFile << endl;
-
-	printSort(insTimes, sizeStart, sizeEnd, textFile, "Insertion");
-	printSort(bubTimes, sizeStart, sizeEnd, textFile, "Bubble");
-	printSort(selTimes, sizeStart, sizeEnd, textFile, "Selection");
-	printSort(shellTimes, sizeStart, sizeEnd, textFile, "Shell");
-	printSort(shellOTimes, sizeStart, sizeEnd, textFile, "Shell/O");
-	printSort(mrgTimes, sizeStart, sizeEnd, textFile, "Merge");
-	printSort(mrgOTimes, sizeStart, sizeEnd, textFile, "Merge/O");
-	printSort(qckTimes, sizeStart, sizeEnd, textFile, "Quick");
-	printSort(qckOTimes, sizeStart, sizeEnd, textFile, "Quick/O");
-	printSort(heapTimes, sizeStart, sizeEnd, textFile, "Heap");
-	printSort(heapOTimes, sizeStart, sizeEnd, textFile, "Heap/O");
-	printSort(rad4Times, sizeStart, sizeEnd, textFile, "Radix/4");
-	printSort(rad8Times, sizeStart, sizeEnd, textFile, "Radix/8");
-
-	textFile.close();
-}
-
-template<typename E>
-void printSort(E arr[], int printStart, int printEnd, ofstream &textFile, std::string sortName)
-{	//Prints sort to console and textfile
-	std::cout << setw(10) << sortName;
-	textFile << sortName;
-	for (int i = printStart; i <= printEnd; i++)
-	{
-		std::cout << setw(12) << arr[i - printStart];
-		textFile << "\t" << arr[i - printStart];
-	}
-	std::cout << endl;
-	textFile << endl;
 }
