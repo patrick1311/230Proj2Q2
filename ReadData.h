8d54f1a032a55b4cb60b3cddf3f5b4707bd9a4c8
pwong34 <pwong34@student.mtsac.edu>
2017-04-19 13:32:08 -0700

    Add files via upload

---
+++ ReadData.h
@@ -0,0 +1,50 @@
+class ReadData 
+{
+private:
+	bool done;
+	int eofLine;
+	int numRuns;
+	std::streamoff pos;
+public:
+	ReadData(){}
+	ReadData(int line, int p)
+	{
+		eofLine = line;
+		pos = p;
+		done = false;
+		numRuns = 0;
+	}
+	inline void Done()
+	{
+		done = true;
+	}
+	inline bool isDone()
+	{
+		return done;
+	}
+	inline int getEofLine()
+	{
+		return eofLine;
+	}
+	inline void setEofLine(int s)
+	{
+		eofLine = s;
+	}
+	inline void setPos(std::streamoff n)
+	{
+		pos = n;
+	}
+	inline std::streamoff getPos()
+	{
+		return pos;
+	}
+	inline void incRuns()
+	{
+		numRuns++;
+	}
+	inline int getRuns()
+	{
+		return numRuns;
+	}
+};
+
