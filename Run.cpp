5df4f990978196bf5bc837efc0f08d622903b323
pwong34 <pwong34@student.mtsac.edu>
2017-04-19 13:47:42 -0700

    Add files via upload

--- Run.cpp
+++ Run.cpp
@@ -1,62 +1,65 @@
-// Heap class
+#include <fstream>
+// Run class
 template <typename E> class Run {
 private:
-	int num;
-	int index;
-	int size;
-	E value;
+	int index;	//Index of record in run
+	int size;	//Number of records in run
+	E value;	//Value of record at index number
+	std::streamoff position;	//Position of record in file
 
 public:
-	Run() { index = 0;}
-	Run(int n, E val)     // Constructor
+	Run() { index = 0; }
+	Run(int ind, int s, E val, std::streamoff pos)     // Constructor
 	{
-		num = n;
+		index = ind;
+		size = s;
 		value = val;
-	}
-	inline int getNum()
-	{
-		return num;
+		position = pos;
 	}
 	inline int getInd()
 	{
 		return index;
 	}
-	inline E getValue()
+	inline E getVal()
 	{
 		return value;
 	}
-	inline void setNum(int n)
+	inline std::streamoff getPos()
 	{
-		num = n;
+		return position;
 	}
-	inline void setValue(E n)
+	inline void incInd()
 	{
-		value = n;
+		index++;
 	}
 	inline void setSize(int n)
 	{
 		size = n;
 	}
-	inline void incIndex()
+	inline void setVal(E n)
 	{
-		index++;
+		value = n;
+	}
+	inline void setPos(std::streamoff pos)
+	{
+		position = pos;
 	}
 	inline bool isDone()
 	{
 		return index == size;
 	}
-
-	bool operator<(const Run &R2)
+	inline bool operator<(const Run &R2)
 	{
 		return value < R2.value;
 	}
-
+	/*
 	void print()
 	{
-		std::cout << "Num: " << num <<std::endl;
+		//std::cout << "Num: " << num << std::endl;
 		std::cout << "Index: " << index << std::endl;
 		std::cout << "Size: " << size << std::endl;
 		std::cout << "Value: " << value << std::endl;
+		std::cout << "Pos: " << position << std::endl;
 		std::cout << std::endl;
-	}
+	}*/
 };
--- heap.h
+++ heap.h
@@ -1,98 +1,108 @@
-// From the software distribution accompanying the textbook
-// "A Practical Introduction to Data Structures and Algorithm Analysis,
-// Third Edition (C++)" by Clifford A. Shaffer.
-// Source code Copyright (C) 2007-2011 by Clifford A. Shaffer.
-//#include "Comp.cpp"
-#include <assert.h>
-#include "swap.cpp"
-#pragma once
-
 // Heap class
-template <typename E, typename Comp> class heap {
+template <typename E> class heap {
 private:
-  E* Heap;          // Pointer to the heap array
-  int maxsize;         // Maximum size of the heap
-  int n;               // Number of elements now in the heap
+	E* Heap;          // Pointer to the heap array
+	int maxsize;         // Maximum size of the heap
+	int n;               // Number of elements now in the heap
 
 public:
-  heap(E* h, int num, int max)     // Constructor
-    { Heap = h;  n = num;  maxsize = max;  buildHeap(); }
-  int size() const       // Return current heap size
-    { return n; }
-  bool isLeaf(int pos) const // True if pos is a leaf
-    { return (pos >= n/2) && (pos < n); }
-  int leftchild(int pos) const
-    { return 2*pos + 1; }    // Return leftchild position
-  int rightchild(int pos) const
-    { return 2*pos + 2; }    // Return rightchild position
-  int parent(int pos) const  // Return parent position
-    { return (pos-1)/2; }
-  void buildHeap()           // Heapify contents of Heap
-    { for (int i=n/2-1; i>=0; i--) siftdown(i); }
+	heap(E* h, int num, int max)     // Constructor
+	{
+		Heap = h;  n = num;  maxsize = max;  buildHeap();
+	}
+	int size() const       // Return current heap size
+	{
+		return n;
+	}
+	bool isLeaf(int pos) const // True if pos is a leaf
+	{
+		return (pos >= n / 2) && (pos < n);
+	}
+	int leftchild(int pos) const
+	{
+		return 2 * pos + 1;
+	}    // Return leftchild position
+	int rightchild(int pos) const
+	{
+		return 2 * pos + 2;
+	}    // Return rightchild position
+	int parent(int pos) const  // Return parent position
+	{
+		return (pos - 1) / 2;
+	}
+	inline void buildHeap()           // Heapify contents of Heap
+	{
+		for (int i = n / 2 - 1; i >= 0; i--) siftdown(i);
+	}
 
-  // Helper function to put element in its correct place
-  void siftdown(int pos) {
-	  while (!isLeaf(pos)) { // Stop if pos is a leaf
-		  int j = leftchild(pos);  int rc = rightchild(pos);
-		  if ((rc < n) && Comp::prior(Heap[rc], Heap[j]))
-			  j = rc;            // Set j to greater child's value
-		  if (Comp::prior(Heap[pos], Heap[j])) return; // Done
-		  swap(Heap, pos, j);
-		  pos = j;             // Move down
-	  }
-  }
+	// Helper function to put element in its correct place
+	inline void siftdown(int pos) {
+		while (!isLeaf(pos)) { // Stop if pos is a leaf
+			int j = leftchild(pos);  int rc = rightchild(pos);
+			if ((rc < n) && Heap[rc] < Heap[j])
+				j = rc;            // Set j to greater child's value
+			if (Heap[pos] < Heap[j]) return; // Done
+			E temp = Heap[pos];
+			Heap[pos] = Heap[j];
+			Heap[j] = temp;
+			pos = j;             // Move down
+		}
+	}
 
-  // Insert "it" into the heap
-  void insert(const E& it) {
-    assert(n < maxsize);
-    int curr = n++;
-    Heap[curr] = it;            // Start at end of heap
-    // Now sift up until curr's parent > curr
-    while ((curr!=0) &&
-           (Comp::prior(Heap[curr], Heap[parent(curr)]))) {
-      swap(Heap, curr, parent(curr));
-      curr = parent(curr);
-    }
-  }
-  // Remove first value
-  E removefirst() {
-    assert (n > 0);
-    swap(Heap, 0, --n);       // Swap first with last value
-    if (n != 0) siftdown(0);  // Siftdown new root val
-    return Heap[n];             // Return deleted value
-  }
+	// Insert "it" into the heap
+	inline void insert(const E& it) {
+		int curr = n++;
+		Heap[curr] = it;            // Start at end of heap
+									// Now sift up until curr's parent > curr
+		while ((curr != 0) && (Heap[curr] < Heap[parent(curr)])) {
+			E temp = Heap[curr];
+			Heap[curr] = Heap[parent(curr)];
+			Heap[parent(curr)] = temp;
+			curr = parent(curr);
+		}
+	}
+	// Remove first value
+	inline E removefirst() {
+		E temp = Heap[0];	// Swap first with last value
+		Heap[0] = Heap[--n];
+		Heap[n] = temp;
+		if (n != 0) siftdown(0);  // Siftdown new root val
+		return Heap[n];             // Return deleted value
+	}
 
-  // Remove and return element at specified position
-  E remove(int pos) {
-    assert((pos >= 0) && (pos < n));
-    if (pos == (n-1)) n--; // Last element, no work to do
-    else
-    {
-      swap(Heap, pos, --n);          // Swap with last value
-      while ((pos != 0) &&
-             (Comp::prior(Heap[pos], Heap[parent(pos)]))) {
-        swap(Heap, pos, parent(pos)); // Push up large key
-        pos = parent(pos);
-      }
-      if (n != 0) siftdown(pos);     // Push down small key
-    }
-    return Heap[n];
-  }
+	// Remove and return element at specified position
+	inline E remove(int pos) {
+		if (pos == (n - 1)) n--; // Last element, no work to do
+		else
+		{
+			E temp = Heap[pos];	// Swap first with last value
+			Heap[pos] = Heap[--n];
+			Heap[n] = temp;
+			while ((pos != 0) && (Heap[pos] < Heap[parent(pos)])) {
+				E temp = Heap[pos];	 // Push up large key
+				Heap[pos] = Heap[parent(pos)];
+				Heap[parent(pos)] = temp;
+				pos = parent(pos);
+			}
+			if (n != 0) siftdown(pos);     // Push down small key
+		}
+		return Heap[n];
+	}
 
-  E getVal(int pos) {
-	  return Heap[pos];
-  }
+	inline E getVal(int pos) {
+		return Heap[pos];
+	}
 
-  E* getValue(int pos) {
-	  return &Heap[pos];
-  }
+	inline E* getValue(int pos) {
+		return &Heap[pos];
+	}
 
-  void setVal(int pos, E val) {
-	  Heap[pos] = val;
-  }
+	inline void setVal(int pos, E val) {
+		Heap[pos] = val;
+	}
 
-  void setHeapSize(int size)
-  {
-	  n = size;
-  }
+	inline void setHeapSize(int size)
+	{
+		n = size;
+	}
 };
